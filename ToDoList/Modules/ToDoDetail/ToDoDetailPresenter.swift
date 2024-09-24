2a1957ccc6f8d175b4ac67e49d9c4b783f691226
Halil Yavuz <halil9913@gmail.com>
2024-09-25 00:19:09 +0300

    added ToDoDetailPresenter

--- ToDoList/Modules/ToDoDetail/ToDoDetailPresenter.swift
+++ ToDoList/Modules/ToDoDetail/ToDoDetailPresenter.swift
@@ -12,3 +12,32 @@ protocol TodoDetailPresenterProtocol: AnyObject {
     func saveTodo(todo: ToDo)
     
 }
+
+final class TodoDetailPresenter: TodoDetailPresenterProtocol {
+    weak var view: TodoDetailViewProtocol?
+    var interactor: TodoDetailInteractorProtocol
+    var router: TodoDetailRouterProtocol
+    var toDo: ToDo?
+    weak var delegate: EditTodoViewDelegateProtocol?
+    init(view: TodoDetailViewProtocol,
+         interactor: TodoDetailInteractorProtocol,
+         router: TodoDetailRouterProtocol,
+         toDo: ToDo?) {
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+        self.toDo = toDo
+    }
+
+    func viewDidLoad() {
+        view?.showTodoDetail(todo: toDo)
+    }
+
+    func saveTodo(todo: ToDo) {
+        interactor.saveTodo(todo)
+        router.dismissDetailView()
+        delegate?.didUpdateTodo(todo: toDo)
+       
+    }
+}
+
